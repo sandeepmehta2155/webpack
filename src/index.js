10c662799e13635a165c7834722518ef4061ea81
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2024-07-01 21:22:08 +0530

    video lecture-v3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import _ from "lodash";
-import Icon from './icon.jpeg';
 import './style.css';
+import printMe from "./print";
 
 function helloWorld() {
     const divElement = document.createElement('div');
+    const btn = document.createElement('button');
+
     divElement.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+
+    btn.onclick = printMe;
 
     divElement.classList.add('hello');
 
-      // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    divElement.appendChild(myIcon);
+    divElement.appendChild(btn);
 
     document.body.appendChild(divElement);
 }
